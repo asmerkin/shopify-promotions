5cf340ef682e3c6bd450106f4b7f47f633bb9eae
Andrés Smerkin <andres@intrepits.com>
2022-01-15 18:34:03 -0300

    Adding a global window object for the promotion runner

--- index.ts
+++ index.ts
@@ -1,19 +1,9 @@
 import PromotionRunner from "./models/PromotionRunner";
 
-// Somehow we need to register this in the window object, so 
-// then it can be called and laoded.
-// window.PromotionRunner = new PromotionRunner(); 
+declare global {
+        interface Window {
+            PromotionRunner: any; 
+        }
+}
 
-// then they could use something like
-
-// PromotionRunner.register({
-//     id: somepromotion,
-//     ...
-// });
-
-// And finally run the promotion executor every time all promos need to be 
-// checked. 
-
-// PromotionRunner.run(cart).then( cart => {
-//     ...do something with the cart output. 
-// }); 
+window.PromotionRunner = new PromotionRunner() || {}; 
