14340a34846075c0302666a0bd6f54403d99a0f0
Andrés Smerkin <perkinbox@gmail.com>
2022-01-17 21:31:54 -0300

    Pulling a new cart if the last action was an addition

--- helpers/processActionsList.ts
+++ helpers/processActionsList.ts
@@ -11,13 +11,21 @@ import CartApi from "./CartApi";
  */
 export async function processActionsList(actions: CartApiAction[]): Promise<Cart> {
     let lastCart = null; 
+    let lastCall = null; 
 
     for( const next of actions) {
+        lastCall = next.action; 
         try {
             lastCart = await CartApi[next.action](next.payload);
         } catch ( error ) {
             console.log( error ); 
         }
+    }
+
+    // the add method returns a list of items, and not a cart, so we call 
+    // the get cart method to pull an updated one if required. 
+    if ( lastCall === 'add') {
+        lastCart = await CartApi.getCart(); 
     }
 
     return lastCart as Cart;
